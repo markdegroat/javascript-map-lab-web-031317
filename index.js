18133d7637c4ee4194dc62ccd5838ee94f15c984
markdegroat <mhdegroat@gmail.com>
2017-04-26 11:55:48 -0400

    Final

--- index.js
+++ index.js
@@ -11,6 +11,21 @@ const robots = [
   { name: 'Ratchet', alliance: null }
 ];
 
+const sortedRobots = robots.map(function(autobot, index) {
+  if (knownDecepticons.includes(autobot.name)){
+  // if (knownDecepticons.includes(robots[index].name)){
+    return Object.assign({}, autobot, {
+      alliance: 'decepticon'
+    })
+  }else{
+    return Object.assign({}, autobot, {
+      alliance: 'autobot'
+    })
+  }
+
+})
+console.log(sortedRobots)
+
 const zebraStripes = [
   { width: 9.12, color: null },
   { width: 5.71, color: null },
@@ -21,3 +36,18 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null }
 ];
+
+const coloredZebraStripes = zebraStripes.map(function(zebra, index) {
+  if (index % 2 === 0){
+    return Object.assign({}, zebra, {
+      color: 'black'
+    })
+  }else{
+    return Object.assign({}, zebra, {
+      color: 'white'
+    })
+  }
+
+})
+
+console.log(coloredZebraStripes)
